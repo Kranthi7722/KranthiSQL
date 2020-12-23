041803ba100d94f25ef22f6a0628a152e1d3b522
Kranthi7722 <kranthi7722@gmail.com>
2020-12-23 09:45:10 -0500

    New Db query added

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,1 +1,7 @@
-select dash_yort 
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [date]
+      ,[platform]
+      ,[spin]
+      ,[total_bet]
+      ,[total_win]
+  FROM [myfirstdb].[dbo].[chal]
