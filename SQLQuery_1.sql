cadb34d4c7dac2b7b282aac4793d716f78841132
Kranthi7722 <kranthi7722@gmail.com>
2020-12-23 00:47:28 -0500

    I have added line " i added lines for demo"

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -211,3 +211,5 @@ End
 GO
 EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=2 , @level0type=N'SCHEMA',@level0name=N'HumanResources', @level1type=N'VIEW',@level1name=N'vEmployee'
 GO
+
+--- i added lines for demo
